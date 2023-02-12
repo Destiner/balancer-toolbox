29363e42c86594108eb81691676f286e4bd41ae6
Timur Badretdinov <destinerx@gmail.com>
2023-02-12 08:15:36 +0400

    Implement "selected pools" option

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -57,10 +57,7 @@
       <div v-else>todo</div>
     </div>
     <div
-      v-if="
-        selectedItem === 'Query batch swap' &&
-        queryBatchSwapSelectedOption === 'ALL_POOLS'
-      "
+      v-if="selectedItem === 'Query batch swap'"
       class="section result"
     >
       <TextView
@@ -102,6 +99,14 @@ import useEnv from '@/composables/useEnv';
 import TokenlistService, { DEFAULT_LIST } from '@/services/tokenlist';
 import { Token } from '@/utils/tokens';
 
+interface Swap {
+  poolId: Address;
+  assetInIndex: BigNumber;
+  assetOutIndex: BigNumber;
+  amount: BigNumber;
+  userData: Address;
+}
+
 const { providerKey } = useEnv();
 
 const { provider } = configureChains(
@@ -128,9 +133,11 @@ async function initSdk(): Promise<void> {
   pools.value = sdk.sor.getPools();
 }
 
+const allTokens = ref<Token[]>([]);
 const tokens = ref<Token[]>([]);
 async function fetchTokens(): Promise<void> {
   const list = await service.get();
+  allTokens.value = list.tokens;
   tokens.value = list.tokens;
 }
 
@@ -148,8 +155,14 @@ const queryBatchSwapOptions = [
   { label: 'Selected path', value: 'SELECTED_PATH' },
 ];
 const queryBatchSwapSelectedOption = ref('ALL_POOLS');
-watch(queryBatchSwapSelectedOption, () => {
+watch(queryBatchSwapSelectedOption, (newValue) => {
   result.value = '';
+  if (newValue === 'ALL_POOLS') {
+    tokens.value = allTokens.value;
+  }
+  if (newValue === 'SELECTED_POOLS') {
+    tokens.value = getPoolTokens(selectedPools.value);
+  }
 });
 
 const pools = ref<SubgraphPoolBase[]>([]);
@@ -158,17 +171,38 @@ const poolOptions = computed(() => {
     return {
       label: pool.tokens
         .map((poolToken) => {
-          const token = tokens.value.find(
+          const token = allTokens.value.find(
             (token) => token.address === poolToken.address,
           );
           return token?.symbol || poolToken.address.substring(0, 8);
         })
         .join('/'),
-      value: pool.address,
+      value: pool.id,
     };
   });
 });
 const selectedPools = ref<string[]>([]);
+watch(selectedPools, (newValue) => {
+  tokens.value = getPoolTokens(newValue);
+});
+
+function getPoolTokens(poolIds: string[]): Token[] {
+  const poolTokenAddresses = poolIds
+    .map((poolId) => {
+      const pool = pools.value.find((pool) => pool.id === poolId);
+      if (!pool) {
+        return [];
+      }
+      return pool.tokensList;
+    })
+    .flat();
+  const uniquePoolTokenAddresses = [...new Set(poolTokenAddresses)];
+  return uniquePoolTokenAddresses
+    .map((address) => {
+      return allTokens.value.find((token) => token.address === address);
+    })
+    .filter((token): token is Token => !!token);
+}
 
 const isExactIn = ref(true);
 const swapTokenInValue = ref('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2');
@@ -189,10 +223,8 @@ const snippet = computed<string | null>(() => {
   if (selectedItem.value !== 'Query batch swap') {
     return null;
   }
-  if (queryBatchSwapSelectedOption.value !== 'ALL_POOLS') {
-    return null;
-  }
-  return `const swapInfo = await sdk.sor.getSwaps(
+  if (queryBatchSwapSelectedOption.value === 'ALL_POOLS') {
+    return `const swapInfo = await sdk.sor.getSwaps(
   '${swapTokenInValue.value}',
   '${swapTokenOutValue.value}',
   ${isExactIn.value ? 'SwapTypes.SwapExactIn' : 'SwapTypes.SwapExactOut'},
@@ -207,8 +239,8 @@ const vaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
 const vault = new ethers.Contract(vaultAddress, vaultAbi, provider);
 const values = await vault.queryBatchSwap(
   ${isExactIn.value ? 'SwapTypes.SwapExactIn' : 'SwapTypes.SwapExactOut'},
-  swapInfo,
-  ['${swapTokenInValue.value}', '${swapTokenOutValue.value}'],
+  swapInfo.swaps,
+  swapInfo.tokenAddresses,
   {
     sender: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
     fromInternalBalance: false,
@@ -217,6 +249,24 @@ const values = await vault.queryBatchSwap(
   }
 )
   `;
+  }
+  if (queryBatchSwapSelectedOption.value === 'SELECTED_POOLS') {
+    const swapData = getSelectedPoolSwapData();
+    return `const vaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
+const vault = new ethers.Contract(vaultAddress, vaultAbi, provider);
+const values = await vault.queryBatchSwap(
+  'SwapTypes.SwapExactIn',
+  [${JSON.stringify(swapData.swaps)}],
+  ['${swapData.tokens.join("', '")}'],
+  {
+    sender: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+    fromInternalBalance: false,
+    recipient: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+    toInternalBalance: false,
+  }
+)`;
+  }
+  return null;
 });
 
 const result = ref('');
@@ -277,8 +327,93 @@ async function queryAllPools(): Promise<void> {
   }
 }
 
+function getSelectedPoolSwapData(): {
+  swaps: Swap[];
+  tokens: string[];
+} {
+  if (swapAmountInValue.value === '') {
+    return {
+      swaps: [],
+      tokens: [],
+    };
+  }
+  const initialAmount = BigNumber.from(swapAmountInValue.value);
+  const swapTokens: string[] = [];
+  const swaps: Swap[] = [];
+  const activePools = selectedPools.value
+    .map((poolId) => {
+      return pools.value.find((pool) => pool.id === poolId);
+    })
+    .filter((pool): pool is SubgraphPoolBase => !!pool);
+  for (let i = 0; i < activePools.length; i++) {
+    const pool = activePools[i];
+    const amount = i === 0 ? initialAmount : BigNumber.from('0');
+    const prevPoolCommonTokens = getIntersection(
+      i === 0 ? [] : activePools[i - 1].tokensList,
+      activePools[i].tokensList,
+    );
+    const nextPoolCommonTokens = getIntersection(
+      activePools[i].tokensList,
+      i === activePools.length - 1 ? [] : activePools[i + 1].tokensList,
+    );
+    const assetIn = i === 0 ? swapTokenInValue.value : prevPoolCommonTokens[0];
+    const assetOut =
+      i === activePools.length - 1
+        ? swapTokenOutValue.value
+        : nextPoolCommonTokens[0];
+    if (!swapTokens.includes(assetIn)) {
+      swapTokens.push(assetIn);
+    }
+    if (!swapTokens.includes(assetOut)) {
+      swapTokens.push(assetOut);
+    }
+    const swap: Swap = {
+      poolId: pool.id as Address,
+      assetInIndex: BigNumber.from(swapTokens.indexOf(assetIn)),
+      assetOutIndex: BigNumber.from(swapTokens.indexOf(assetOut)),
+      amount,
+      userData: '0x',
+    };
+    swaps.push(swap);
+  }
+  return { swaps, tokens: swapTokens };
+}
+
 async function querySelectedPools(): Promise<void> {
-  console.log('todo');
+  const { swaps, tokens } = getSelectedPoolSwapData();
+  try {
+    const values = await readContract({
+      address: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+      abi: vaultAbi,
+      functionName: 'queryBatchSwap',
+      args: [
+        SwapTypes.SwapExactIn,
+        swaps.map((swap) => {
+          return {
+            poolId: swap.poolId,
+            assetInIndex: swap.assetInIndex,
+            assetOutIndex: swap.assetOutIndex,
+            amount: swap.amount,
+            userData: swap.userData,
+          };
+        }),
+        tokens as Address[],
+        {
+          sender: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+          fromInternalBalance: false,
+          recipient: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+          toInternalBalance: false,
+        },
+      ],
+    });
+    result.value = JSON.stringify(values.map((value) => value.toString()));
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function getIntersection<T>(a: T[], b: T[]): T[] {
+  return a.filter((value) => b.includes(value));
 }
 </script>
 
