370e66a3f44f206e842fd0cd9b68a9e7c8778af1
Timur Badretdinov <destinerx@gmail.com>
2023-03-03 20:51:41 +0400

    TextView UI improvements

---
+++ src/components/CopyButton.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    class="button"
+    :class="{ compact }"
+    @click="copy"
+  >
+    <IconClipboard
+      v-if="ready"
+      class="icon"
+      :class="{ compact }"
+    />
+    <IconSuccess
+      v-else
+      class="icon"
+      :class="{ compact }"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTimeout } from '@vueuse/core';
+import { onMounted } from 'vue';
+
+import IconClipboard from '@/components/__common/icon/Clipboard.vue';
+import IconSuccess from '@/components/__common/icon/Success.vue';
+
+const props = withDefaults(
+  defineProps<{
+    value: string;
+    compact?: boolean;
+  }>(),
+  {
+    compact: false,
+  },
+);
+
+onMounted(() => {
+  stop();
+});
+
+const { ready, start, stop } = useTimeout(2000, { controls: true });
+
+function copy(): void {
+  navigator.clipboard.writeText(props.value);
+  start();
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 8px;
+  border: 1px solid var(--color-border-primary);
+  border-radius: var(--border-radius-medium);
+  cursor: pointer;
+}
+
+.button:hover {
+  border: 1px solid var(--color-border-secondary);
+}
+
+.button.compact {
+  padding: 2px;
+  border: none;
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+  color: var(--color-text-secondary);
+}
+
+.icon.compact {
+  width: 12px;
+  height: 12px;
+}
+</style>
--- src/components/TextView.vue
+++ src/components/TextView.vue
@@ -1,19 +1,76 @@
 <template>
-  <textarea
-    :value="value"
-    readonly
-    rows="15"
-  />
+  <div class="wrapper">
+    <textarea
+      :value="value"
+      readonly
+      spellcheck="false"
+      :rows="compact ? 10 : 16"
+    />
+    <CopyButton
+      class="button"
+      :value="value"
+    />
+    <div
+      v-if="isLoading"
+      class="loading"
+    >
+      <LoadingIndicator />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import CopyButton from './CopyButton.vue';
+import LoadingIndicator from './LoadingIndicator.vue';
+
 defineProps<{
   value: string;
+  compact?: boolean;
+  isLoading?: boolean;
 }>();
 </script>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+}
+
 textarea {
-  font-family: 'Courier New', Courier, monospace;
+  width: 100%;
+  margin: 0;
+  padding: 8px;
+  overflow-y: scroll;
+  border: 1px solid var(--color-border-primary);
+  border-radius: var(--border-radius-medium);
+  outline: none;
+  background: var(--color-bg-secondary);
+  box-shadow: var(--shadow-medium-inset);
+  color: var(--color-text-primary);
+  font-family: var(--font-mono);
+  font-size: var(--font-size-small);
+  resize: none;
+}
+
+textarea.error {
+  border: 1px solid var(--color-error);
+}
+
+.button {
+  display: none;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  background: var(--color-bg-primary);
+}
+
+.wrapper:hover .button {
+  display: initial;
+}
+
+.loading {
+  position: absolute;
 }
 </style>
