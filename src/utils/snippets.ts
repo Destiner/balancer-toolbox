d0e25c5c1ba1a6cfe604c8e115c0c564be6cc4e7
Timur Badretdinov <destinerx@gmail.com>
2023-03-03 20:18:07 +0400

    Replace "any" with "unknown"

--- src/utils/snippets.ts
+++ src/utils/snippets.ts
@@ -33,7 +33,7 @@ const values = await vault.queryBatchSwap(
 }
 
 function getQueryBatchSwapSelectedPoolsSnippet(
-  swaps: any,
+  swaps: unknown,
   tokens: string[],
 ): string {
   return `const vaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
